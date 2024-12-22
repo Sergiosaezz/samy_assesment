86eb24c7325ad154469c0a1033f3aa8f568620db
Sergio Saez Trigueros <sergiosaez94@gmail.com>
2024-12-23 00:14:36 +0100

    feat: add apollo client and first tryouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,55 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_IMAGES = gql`
+  query GetImages($title: String, $first: Int, $after: String) {
+    images(title: $title, first: $first, after: $after) {
+      edges {
+        node {
+          id
+          title
+          picture
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+    }
+  }
+`;
+
 function App() {
+  const { loading, error, data, fetchMore } = useQuery(GET_IMAGES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const loadMore = () => {
+    if (data.images.pageInfo.hasNextPage) {
+      fetchMore({
+        variables: { after: data.images.pageInfo.endCursor },
+      });
+    }
+  };
+
   return (
     <>
-      <h1 className="text-3xl font-bold text-blue-900">Hola</h1>
+      <h1 className="text-3xl font-bold text-blue-900 mb-10 text-center pt-3">
+        SAMY ALLIANCE assessment
+      </h1>
+      <div>
+        <ul className="flex flex-wrap gap-3">
+          {data.images.edges.map(({ node }) => (
+            <li className="flex flex-col items-center" key={node.id}>
+              <img src={node.picture} alt={node.title} />
+              <p>{node.title}</p>
+            </li>
+          ))}
+        </ul>
+        {data.images.pageInfo.hasNextPage && (
+          <button onClick={loadMore}>Load More</button>
+        )}
+      </div>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { API_URL } from "./config.ts";
+
+const client = new ApolloClient({
+  uri: API_URL,
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>
 );
