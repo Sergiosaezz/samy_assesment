63269d682f8abd56dae6db2b844b6ba8c440ac8c
Sergio Saez Trigueros <sergiosaez94@gmail.com>
2024-12-23 22:04:54 +0100

    feat: header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
 export const Header = () => (
-  <header className="pt-3">
-    <h1 className="text-3xl font-bold text-blue-900 mb-10 text-center ">
-      SAMY ALLIANCE assessment
-    </h1>
+  <header className="px-9 py-9 md:px-24 flex gap-3 flex-col items-start md:items-center md:flex-row justify-between">
+    <img src="logo.svg" alt="samy logo" className="h-3.5 md:h-6" />
+    <div className="relative self-center">
+      <input
+        type="text"
+        placeholder="You're looking for something?"
+        className="pl-10 py-2 w-64 h-8 pr-4 rounded-2xl bg-[#F2F2F2] placeholder-[#A1A1A1] text-sm"
+      />
+      <img
+        src="search-icon.svg"
+        alt="Search icon"
+        className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5"
+      />
+    </div>
   </header>
 );
