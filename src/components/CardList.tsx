2f1a57438ccc778130653661503223fbb4e00bfb
Sergio Saez Trigueros <sergiosaez94@gmail.com>
2024-12-26 00:35:59 +0100

    feat: empty state of card list

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -10,21 +10,25 @@ type Props = {
   handleLike: (image: ImageItem["id"]) => Promise<void>;
 };
 
-export const CardList = ({ list, loadMore, hasMore, handleLike }: Props) => (
-  <InfiniteScroll
-    dataLength={list.length}
-    next={loadMore}
-    hasMore={hasMore}
-    loader={<Loader />}
-    style={{ overflow: "hidden" }}
-  >
-    <ul
-      className="flex flex-wrap justify-center gap-5 lg:justify-between"
-      data-test="image-list"
+export const CardList = ({ list, loadMore, hasMore, handleLike }: Props) => {
+  if (!list.length)
+    return <h1 className="text-center pb-10">No data available</h1>;
+  return (
+    <InfiniteScroll
+      dataLength={list.length}
+      next={loadMore}
+      hasMore={hasMore}
+      loader={<Loader />}
+      style={{ overflow: "hidden" }}
     >
-      {list.map(({ node }) => (
-        <Card item={node} key={node.id} handleLike={handleLike} />
-      ))}
-    </ul>
-  </InfiniteScroll>
-);
+      <ul
+        className="flex flex-wrap justify-center gap-5 lg:justify-between"
+        data-test="image-list"
+      >
+        {list.map(({ node }) => (
+          <Card item={node} key={node.id} handleLike={handleLike} />
+        ))}
+      </ul>
+    </InfiniteScroll>
+  );
+};
